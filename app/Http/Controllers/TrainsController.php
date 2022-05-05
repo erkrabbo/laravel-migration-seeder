cf2ec3f5052a26a3b6b7e9b15914ef7d371d6ff7
Luca Andaloro <luca.andaloro4@gmail.com>
2022-05-06 01:43:14 +0200

    meglio

--- app/Http/Controllers/TrainsController.php
+++ app/Http/Controllers/TrainsController.php
@@ -15,12 +15,13 @@ class TrainsController extends Controller
      */
     public function index()
     {
-        $today = date_create(now())
+        $today = date_create(today())
+            // ->setTime(00,00,00)
             ->format('Y-m-d H:i:s');
         // date_time_set($today, 15, 20, 28);
         // $today->setDate(2022,05,06);
         // $today = $today->format('Y-m-d H:i:s');
-        // dd($today);
+        dd($today);
         $trains = Train::where('departure_timestamp','>=', $today)
             ->orderBy('departure_timestamp')
             ->get();
